7763efb6f0db9b0bbb96b7b93e4b630e4125cdee
biplobDlab <biplob@dreamerzlab.com>
2022-12-13 14:51:16 +0600

    Npm Added

--- index.js
+++ index.js
@@ -1,42 +1,53 @@
 
 
+
+const modelViewerTexture1 = document.querySelector("model-viewer");
+// var pathList = [];
 // async function exportGLB()
 // {
 //   const modelViewer = document.getElementById("model-viewer");
 //   const glTF = await modelViewer.exportScene();
-//   modelViewerTexture1.model = glTF;
+//   const file = new File([glTF], "export.glb");
+//   var path =  URL.createObjectURL(file);
+//   if(!pathList.includes(path))
+//   {
+//     pathList.push(path);
+//   }
+//   document.getElementById("model-viewer").src = path;
+//   console.log(path);
+//   console.log(pathList);
+//   //return glTF;
 // }
 
-// const modelViewerTexture1 = document.querySelector("model-viewer");
+modelViewerTexture1.addEventListener("load", () => {
 
-// modelViewerTexture1.addEventListener("load", () => {
+  console.log(modelViewerTexture1.availableVariants);
+  const material = modelViewerTexture1.model.materials[0];
 
-//   const material = modelViewerTexture1.model.materials[0];
+  const createAndApplyTexture = async (channel, value) => {
+    const texture = await modelViewerTexture1.createTexture(value);
+    if (channel.includes('base') || channel.includes('metallic')) {
+      material.pbrMetallicRoughness[channel].setTexture(texture);
+    } else {
+      material[channel].setTexture(texture);
+    }
+  }
+  const highlightedItems = document.querySelectorAll(".texture-button");
 
-//   const createAndApplyTexture = async (channel, value) => {
-//     const texture = await modelViewerTexture1.createTexture(value);
-//     if (channel.includes('base') || channel.includes('metallic')) {
-//       material.pbrMetallicRoughness[channel].setTexture(texture);
-//     } else {
-//       material[channel].setTexture(texture);
-//     }
-//   }
-//   const highlightedItems = document.querySelectorAll(".texture-button");
+  highlightedItems.forEach((texture_button) => {
+    texture_button.addEventListener('click', (event) => {      
+      createAndApplyTexture('baseColorTexture',texture_button.getAttribute("texture_value"));
+    });
+  });
 
-//   highlightedItems.forEach((texture_button) => {
-//     texture_button.addEventListener('click', (event) => {
-//       createAndApplyTexture('baseColorTexture',texture_button.value);
-//     });
-//   });
-
-//   //const buttons = document.querySelectorAll(".arbutton");
-//   // buttons.forEach((ar_button) => {
-//   //   ar_button.addEventListener('click', (event) => {
-//   //     //modelViewerTexture1.getElementById("ar-button").click();
-//   //   });
-//   // });
-//   createAndApplyTexture('baseColorTexture', document.getElementById('texture-green-orange1').value);
-// });
+  //const buttons = document.querySelectorAll(".arbutton");
+  // buttons.forEach((ar_button) => {
+  //   ar_button.addEventListener('click', (event) => {
+  //     //modelViewerTexture1.getElementById("ar-button").click();
+  //   });
+  // });
+  createAndApplyTexture('baseColorTexture', document.getElementById('texture-green-orange1').getAttribute("texture_value"));
+});
 
 
 function displeCheck(e) {
@@ -64,10 +75,16 @@ function displeCheck(e) {
   
 }
 
-const highlightedItems = document.querySelectorAll(".texture-button");
+// const highlightedItems = document.querySelectorAll(".texture-button");
 
-  highlightedItems.forEach((texture_button) => {
-    texture_button.addEventListener('click', (event) => {
-      document.getElementById("model-viewer").src = texture_button.value;
-    });
-  });
+// highlightedItems.forEach((texture_button) => {
+//     texture_button.addEventListener('click', (event) => {
+//       document.getElementById("model-viewer").src = texture_button.value;
+//     });
+//   });
+  // const buttons = document.querySelectorAll(".arbutton");
+  // buttons.forEach((ar_button) => {
+  //   ar_button.addEventListener('click', (event) => {
+  //     exportGLB();
+  //   });
+  // });
